226adf90262280d92238c7ee9a977acc1da55219
Meybelin <perdomomeybel@gmail.com>
2024-01-25 21:39:16 -0600

    last details

--- js/script.js
+++ js/script.js
@@ -33,7 +33,50 @@ function cleanInputString(str) {
     />`;
     targetInputContainer.insertAdjacentHTML("beforeend",HTMLString);
   }
+    function calculateCalories(e) {
+        e.preventDefault();
+        isError = false;
 
+      // Call Inputs for various elements
+      // for breakfast
+        const breakfastNumberInputs = document.querySelectorAll('#breakfast input[type=number]');
+        const breakfastCalories = getCaloriesFromInputs(breakfastNumberInputs);
+      // for luch
+        const lunchNumberInputs = document.querySelectorAll('#lunch input[type=number]');
+        const lunchCalories = getCaloriesFromInputs(lunchNumberInputs);
+      // for dinner
+        const dinnerNumberInputs = document.querySelectorAll('#dinner input[type=number]');
+        const dinnerCalories = getCaloriesFromInputs(dinnerNumberInputs);
+      // for snack
+        const snacksNumberInputs = document.querySelectorAll('#snacks input[type=number]');
+        const snacksCalories = getCaloriesFromInputs(snacksNumberInputs);
+      // for exercise
+        const exerciseNumberInputs = document.querySelectorAll('#exercise input[type=number]');
+        const exerciseCalories = getCaloriesFromInputs(exerciseNumberInputs);
+        
+      // Declare a budgetCalories variable and pass an array containing budgetNumberInput
+        const budgetCalories = getCaloriesFromInputs([budgetNumberInput]);
+        
+      // Check the truthiness of the global error flag
+        if (isError) {
+          // End the function execution
+          return calculateCalories();
+        }
+      // Declare a consumedCalories variable and assign it the sum
+        const consumedCalories = breakfastCalories + lunchCalories + dinnerCalories + snacksCalories;
+        const remainingCalories = budgetCalories - consumedCalories + exerciseCalories;
+
+        const surplusOrDeficit = remainingCalories >= 0 ? 'Surplus' : 'Deficit';
+      // Assign an empty template literal to the innerHTML property
+      output.innerHTML = `
+        <span class="${surplusOrDeficit.toLowerCase()}">${Math.abs(remainingCalories)} Calorie ${surplusOrDeficit}</span>
+        <hr>
+        <p>${budgetCalories} Calories Budgeted</p>
+        <p>${consumedCalories} Calories Consumed</p>
+        <p>${exerciseCalories} Calories Burned</p>
+    `;
+    output.classList.remove('hide');
+      }
   function getCaloriesFromInputs(list) {
     let calories = 0;
     
@@ -49,40 +92,21 @@ function cleanInputString(str) {
       calories += Number(currVal); 
     }
     return calories;
-
-    function calculateCalories(e) {
-      e.preventDefault();
-      isError = false;
-
-    // Call Inputs for various elements
-    // for breakfast
-      const breakfastNumberInputs = document.querySelectorAll('#breakfast input[type=number]');
-      const breakfastCalories = getCaloriesFromInputs(breakfastNumberInputs);
-    // for luch
-      const lunchNumberInputs = document.querySelectorAll('#lunch input[type=number]');
-      const lunchCalories = getCaloriesFromInputs(lunchNumberInputs);
-    // for dinner
-      const dinnerNumberInputs = document.querySelectorAll('#dinner input[type=number]');
-      const dinnerCalories = getCaloriesFromInputs(dinnerNumberInputs);
-    // for snack
-      const snacksNumberInputs = document.querySelectorAll('#snacks input[type=number]');
-      const snacksCalories = getCaloriesFromInputs(snacksNumberInputs);
-    // for exercise
-      const exerciseNumberInputs = document.querySelectorAll('#exercise input[type=number]');
-      const exerciseCalories = getCaloriesFromInputs(exerciseNumberInputs);
-      
-    // Declare a budgetCalories variable and pass an array containing budgetNumberInput
-      const budgetCalories = getCaloriesFromInputs([budgetNumberInput]);
-      
-    // Check the truthiness of the global error flag
-      if (isError) {
-        // End the function execution
-        return calculateCalories();
-      }
-    // Declare a consumedCalories variable and assign it the sum
-      let consumedCalories = breakfastCalories + lunchCalories + dinnerCalories + snacksCalories;
-
-    
-    }
-
+   
   }
+
+  function clearForm(){
+    const inputContainers = Array.from(document.querySelectorAll('.input-container'));
+    for (let i = 0; i< inputContainers.length; i++) {
+      inputContainers[i].innerHTML = '';
+  
+    }
+    budgetNumberInput.value ="";
+    output.innerText = "";
+    output.classList.add('hide');
+    clearButton.addEventListener("click", clearForm)
+  }
+
+  addEntryButton.addEventListener("click", addEntry);
+  calorieCounter.addEventListener("submit", calculateCalories());
+
